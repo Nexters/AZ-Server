b81177848067d7e5e854651d29adf3813f409117
rayoungji <dudwl7676@naver.com>
2020-07-10 16:03:15 +0900

    YouTuGo 프로젝트 초기 설정

---
+++ youtugo-api/src/main/java/org/nexters/az/YouTuGoApiApplication.java
@@ -0,0 +1,11 @@
+package org.nexters.az;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTuGoApiApplication {
+    public static void main(String[] args){
+        SpringApplication.run(YouTuGoApiApplication.class,args);
+    }
+}
