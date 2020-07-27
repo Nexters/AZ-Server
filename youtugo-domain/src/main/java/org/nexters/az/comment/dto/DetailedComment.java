41fadc1b5818db56dc2267e6dd2366415e9aeff3
rayoungji <dudwl7676@naver.com>
2020-07-27 22:27:18 +0900

    #20 코멘트 API 수정 DetailedComment 변수명 수정

--- youtugo-domain/src/main/java/org/nexters/az/comment/dto/DetailedComment.java
+++ youtugo-domain/src/main/java/org/nexters/az/comment/dto/DetailedComment.java
@@ -10,16 +10,16 @@ import java.util.List;
 @Getter
 public class DetailedComment {
     private Long id;
-    private LocalDateTime created_at;
-    private LocalDateTime modify_at;
+    private LocalDateTime created_At;
+    private LocalDateTime modify_At;
     private Long postId;
     private String writer;
     private String content;
 
     public DetailedComment(Comment comment){
         this.id=comment.getId();
-        this.created_at=comment.getCreatedDate();
-        this.modify_at=comment.getModifiedDate();
+        this.created_At=comment.getCreatedDate();
+        this.modify_At=comment.getModifiedDate();
         this.postId=comment.getPost().getId();
         this.writer=comment.getWriter().getNickname();
         this.content=comment.getComment();
