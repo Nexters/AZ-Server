e2a3faf70c05a323cc7d71e258ec2b7c101b64f5
rayoungji <dudwl7676@naver.com>
2020-07-30 21:29:27 +0900

    #33 CI/CD 연동 - deploy.yml 수정 : develop 브랜치에 pull&request가 왔을 때 빌드 실행 - appspec.yml 수정 : start.sh 파일 실행 명령 추가 - start.sh 추가 : 배포 자동화 스크립트 추가 - secretKey & accessKey 수정

---
+++ scripts/start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ec2-user/app/deploy
+PROJECT_NAME=AZ-Server
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/*.jar $REPOSITORY/zip
+
+echo "> 구동중인 애플리케이션 pid가 있다면 죽입니다"
+fuser -k -n tcp 8080
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/zip/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar -Dspring.config.location=/home/ec2-user/app/application-real-db.properties $JAR_NAME > $REPOSITORY/zip/nohup.out 2>&1 &
