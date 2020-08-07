82d950e614e3bf7887a91d900368bb0b00435ee7
chlalstjd430 <cms3136@gmail.com>
2020-08-07 20:29:43 +0900

    feature/55 - 등급 조건 조회 API 수정
    작성자 : 최민성
    
    - response에 퍼센트 / 문구 추가

--- youtugo-domain/src/main/java/org/nexters/az/user/dto/RatingForPromotion.java
+++ youtugo-domain/src/main/java/org/nexters/az/user/dto/RatingForPromotion.java
@@ -7,10 +7,27 @@ import org.nexters.az.user.entity.Rating;
 
 @AllArgsConstructor
 @Getter
-@Builder
 public class RatingForPromotion {
     private Rating currentRating;
     private Rating nextRating;
     private int postCountForPromotion;
     private int commentCountForPromotion;
+    private float progress;
+    private String message;
+
+    @Builder
+    public RatingForPromotion(
+        Rating currentRating,
+        Rating nextRating,
+        int postCountForPromotion,
+        int commentCountForPromotion,
+        float progress
+    ) {
+        this.currentRating = currentRating;
+        this.nextRating = nextRating;
+        this.postCountForPromotion = postCountForPromotion;
+        this.commentCountForPromotion = commentCountForPromotion;
+        this.message = currentRating.getMessage();
+        this.progress = progress;
+    }
 }
--- youtugo-domain/src/main/java/org/nexters/az/user/service/UserService.java
+++ youtugo-domain/src/main/java/org/nexters/az/user/service/UserService.java
@@ -1,5 +1,6 @@
 package org.nexters.az.user.service;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.nexters.az.auth.security.SHA256Util;
 import org.nexters.az.comment.repository.CommentRepository;
@@ -62,36 +63,39 @@ public class UserService {
 
     public RatingForPromotion findRatingForPromotion(int postCount, int commentCount) {
         Rating currentRating;
-        Rating nextRating = null;
+        Rating nextRating;
         int postCountForPromotion = 0;
         int commentCountForPromotion = 0;
 
-        if (postCount < 5 || commentCount < 3) {
+        if (postCount < Rating.ASSISTANT_MANAGE.getPostCount() || commentCount < Rating.ASSISTANT_MANAGE.getCommentCount()) {
             currentRating = Rating.NEW_RECRUIT;
             nextRating = Rating.ASSISTANT_MANAGE;
-            postCountForPromotion = 5 - postCount;
-            commentCountForPromotion = 3 - commentCount;
+            postCountForPromotion = Rating.ASSISTANT_MANAGE.getPostCount() - postCount;
+            commentCountForPromotion = Rating.ASSISTANT_MANAGE.getCommentCount() - commentCount;
+
         }
-        else if (postCount < 10 || commentCount < 15) {
+        else if (postCount < Rating.DEPARTMENT_HEAD.getPostCount() || commentCount < Rating.DEPARTMENT_HEAD.getCommentCount()) {
             currentRating = Rating.ASSISTANT_MANAGE;
             nextRating = Rating.DEPARTMENT_HEAD;
-            postCountForPromotion = 10 - postCount;
-            commentCountForPromotion = 15 - commentCount;
+            postCountForPromotion = Rating.DEPARTMENT_HEAD.getPostCount() - postCount;
+            commentCountForPromotion = Rating.DEPARTMENT_HEAD.getCommentCount() - commentCount;
         }
-        else if (postCount < 50 || commentCount < 50) {
+        else if (postCount < Rating.MANAGING_DIRECTOR.getPostCount() || commentCount < Rating.MANAGING_DIRECTOR.getCommentCount()) {
             currentRating = Rating.DEPARTMENT_HEAD;
             nextRating = Rating.MANAGING_DIRECTOR;
-            postCountForPromotion = 50 - postCount;
-            commentCountForPromotion = 50 - commentCount;
+            postCountForPromotion = Rating.MANAGING_DIRECTOR.getPostCount() - postCount;
+            commentCountForPromotion = Rating.MANAGING_DIRECTOR.getCommentCount() - commentCount;
         }
-        else if (postCount < 100 || commentCount < 100) {
+        else if (postCount < Rating.BOSS.getPostCount() || commentCount < Rating.BOSS.getCommentCount()) {
             currentRating = Rating.MANAGING_DIRECTOR;
             nextRating = Rating.BOSS;
-            postCountForPromotion = 100 - postCount;
-            commentCountForPromotion = 100 - commentCount;
+            postCountForPromotion = Rating.BOSS.getPostCount() - postCount;
+            commentCountForPromotion = Rating.BOSS.getCommentCount() - commentCount;
         }
         else {
             currentRating = Rating.BOSS;
+            nextRating = Rating.BOSS;
+
         }
 
         postCountForPromotion = Math.max(postCountForPromotion, 0);
@@ -103,6 +107,16 @@ public class UserService {
                 .nextRating(nextRating)
                 .commentCountForPromotion(commentCountForPromotion)
                 .postCountForPromotion(postCountForPromotion)
+                .progress(makeProgress(postCountForPromotion, commentCountForPromotion, nextRating))
                 .build();
+    }
+
+    private float makeProgress(int postCount, int commentCount, Rating rating) {
+        if (rating == Rating.BOSS) return 1;
+        
+        int postCountForPromotion = rating.getPostCount();
+        int commentCountForPromotion = rating.getCommentCount();
+
+        return (float)((postCountForPromotion - postCount) + (commentCountForPromotion - commentCount)) / (float)(postCountForPromotion + commentCountForPromotion);
     }
 }
