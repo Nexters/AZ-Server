c964c959db57f1ad1e4b578ed459645c4dde538b
MinSeong <cms3136@gmail.com>
2020-08-07 20:32:49 +0900

    feature/55 - 등급 조건 조회 API 수정 (#59)
    작성자 : 최민성
    
    - response에 퍼센트 / 문구 추가

--- youtugo-domain/src/main/java/org/nexters/az/user/entity/Rating.java
+++ youtugo-domain/src/main/java/org/nexters/az/user/entity/Rating.java
@@ -4,15 +4,21 @@ import lombok.Getter;
 
 @Getter
 public enum Rating {
-    NEW_RECRUIT("신입사원"),
-    ASSISTANT_MANAGE("대리"),
-    DEPARTMENT_HEAD("부장"),
-    MANAGING_DIRECTOR("상무"),
-    BOSS("사장");
+    NEW_RECRUIT("신입사원", "어이신입ㅋ\n유머 좁 하나?", 0, 0),
+    ASSISTANT_MANAGE("대리", "어이대리~\n반려다 니 유머", 5, 3),
+    DEPARTMENT_HEAD("부장", "부장님ㅋㅎ\nMZ세대는 달라^^", 10 ,15),
+    MANAGING_DIRECTOR("상무", "이야~상무님!\n나이스샷ㅎ", 50, 50),
+    BOSS("사장", "사장님.\n탁월하십니다.", 100, 100);
 
     private String name;
+    private String message;
+    private int postCount;
+    private int commentCount;
 
-    Rating(String name) {
+    Rating(String name, String message, int postCount, int commentCount) {
         this.name = name;
+        this.message = message;
+        this.postCount = postCount;
+        this.commentCount = commentCount;
     }
 }
