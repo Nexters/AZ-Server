a898a13163c764b5a9cb93f0bae0140bbdea74d5
MinSeong <cms3136@gmail.com>
2020-08-13 11:01:13 +0900

    feature/58 - 등급 조회 API 테스트 코드 작성 (#76)
    작성자 : 최민성
    
    - 등급별 조건 성립 테스트 코드 작성

--- youtugo-application/src/main/java/org/nexters/az/post/controller/PostController.java
+++ youtugo-application/src/main/java/org/nexters/az/post/controller/PostController.java
@@ -20,7 +20,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
